069cf55b237932abceac06fb90d3490b35ee038c
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2026-09-16 16:18:53 -0300

    Adding out-of-power icon to factory

--- scripts/buildings/factory_building.gd
+++ scripts/buildings/factory_building.gd
@@ -5,13 +5,15 @@ const SIZE := Vector2(59, 49)
 
 @onready
 var power_progress_bar: ProgressBar = %PowerProgressBar
+@onready
+var out_of_power: Node2D = %OutOfPower
 
 var _area := Rect2(-SIZE / 2.0, SIZE)
 
 func _process(delta: float) -> void:
     var component := find_component(BuildingComponent.Kind.POWER_CONSUMER) as PowerConsumerComponent
     if component != null:
-        component.try_consume(1.0 * delta)
+        out_of_power.visible = not component.try_consume(1.0 * delta)
 
         power_progress_bar.max_value = component.power_capacity
         power_progress_bar.value = component.power_available
